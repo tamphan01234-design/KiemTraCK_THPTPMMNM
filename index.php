1e21d0adc0c9674f0ebefb07b47ac3ffcf96d9cd
tamphan01234-design <tamphan01234@gmail.com>
2025-12-18 12:51:16 +0700

    first

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Bài Kiểm Tra Git</title>
+</head>
+<body>
+    <h1> Bài Kiểm tra - DH52201404 </h1>
+
+    <div style="margin-top: 20px; padding: 15px; border: 1px solid #ccc;">
+        <h3>Thông tin chi tiết:</h3>
+        <p><strong>Họ và tên:</strong> Phan Huỳnh Nhất Tâm</p>
+        <p><strong>Môn học:</strong> Thực hành PTPM Mã Nguồn Mở</p>
+        <p><strong>Trạng thái:</strong> Đang kiểm tra quá trình Deploy</p>
+    </div>
+</body>
+</html>
