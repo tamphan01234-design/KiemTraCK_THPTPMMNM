589dda02ec6c99be9d97315188c0f2900b4aabef
tamphan01234-design <tamphan01234@gmail.gmailcom>
2025-12-18 15:32:30 +0700

    Thêm action để xem quá trình deploy và build

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 <html lang="vi">
 <head>
     <meta charset="UTF-8">
-    <title>Bài Kiểm Tra Git</title>
+    <title>Kiểm Tra Cuối Kì</title>
 </head>
 <body>
-    <h1> Bài Kiểm tra - DH52201404 </h1>
+    <h1> Thi Thứ 5-Ca 4-Phan Huỳnh Nhất Tâm </h1>
 
     <div style="margin-top: 20px; padding: 15px; border: 1px solid #ccc;">
         <h3>Thông tin chi tiết:</h3>
         <p><strong>Họ và tên:</strong> Phan Huỳnh Nhất Tâm</p>
         <p><strong>Môn học:</strong> Thực hành PTPM Mã Nguồn Mở</p>
-        <p><strong>Trạng thái:</strong> Đang kiểm tra quá trình Deploy</p>
+        <p><strong>Trạng thái:</strong> Thành Công</p>
     </div>
 </body>
 </html>
